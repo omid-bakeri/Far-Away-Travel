f7529535942bcbbcf2e33157d445d9a0471994e0
Robert.minia <omid-bakeri>
2023-07-02 13:00:54 +0430

    file updated

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+
+export default function App() {
+  return <FarAway />;
+}
+
+function FarAway() {
+  return (
+    <>
+      <div className="">Hello</div>
+    </>
+  );
+}
